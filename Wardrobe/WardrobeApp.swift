5f2b1185ce60c2ebfed8f64a57772f9bbc5cbec7
Jason Lacy <jdlacy@gmail.com>
2022-04-02 10:38:29 -0700

    Initial Commit

---
+++ Wardrobe/WardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeApp.swift
+//  Wardrobe
+//
+//  Created by Jason on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
