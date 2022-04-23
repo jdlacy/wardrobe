95a245d958d0275500eba6f1551faf75547b784c
Sean Jones <Sjones424206@gmail.com>
2022-04-23 15:48:24 -0700

    FirebaseAuth working.

--- Wardrobe/ContentView.swift
+++ Wardrobe/ContentView.swift
@@ -34,7 +34,7 @@ class APPViewModel: ObservableObject {
     
     func signUp(email: String, password: String) {
         auth.createUser(withEmail: email,
-                        password: password) {[weak self] result,
+                        password: password) { [weak self] result,
                 error in
             guard result != nil, error == nil else {
                 return
@@ -85,8 +85,8 @@ struct ContentView: View {
 }
 
 struct SignInView: View {
-    @State var email = " "
-    @State var password = " "
+    @State var email = ""
+    @State var password = ""
     @EnvironmentObject var viewModel: APPViewModel
     
    var body: some View {
@@ -140,8 +140,8 @@ struct SignInView: View {
 }
 
 struct SignUpView: View {
-    @State var email = " "
-    @State var password = " "
+    @State var email = ""
+    @State var password = ""
     @EnvironmentObject var viewModel: APPViewModel
     
    var body: some View {
